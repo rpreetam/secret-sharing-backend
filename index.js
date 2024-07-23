7b23c3cacd321d6408791a0363a37bdb935637c0
rpreetam <122427885+rpreetam@users.noreply.github.com>
2024-07-23 08:52:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 
-app.use(cors({
-    origin: 'https://secret-sharing-sigma.vercel.app',
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'auth-token']
-}));
+app.use(cors());
 
-app.options('*', cors());
+
 
 app.use('/api/auth', require('./routs/auth'))
 app.use('/api/secrets', require('./routs/secrets'))
